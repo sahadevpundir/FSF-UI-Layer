360bb7e328a103188da2f82a282c4c8083e4e8c0
Sahadev Pundir <sahadevpundir@gmail.com>
2021-11-18 16:50:28 +0530

    commit

---
+++ Assignment3/index.js
@@ -0,0 +1,6 @@
+const calculator=require('./calculator')
+
+console.log("Addition is : "+calculator.add(50,10));
+console.log("Subtraction is : "+calculator.sub(50,10));
+console.log("Multiplication is : "+calculator.mul(50,10));
+console.log("Division is : "+calculator.div(50,10));
